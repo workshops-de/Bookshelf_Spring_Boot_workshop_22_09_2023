d34c57e7fdbb82fcd2a5d8d1aa71702f76ae4146
Roland Weisleder <roland@rweisleder.de>
2023-09-22 13:05:09 +0200

    Initial commit

---
+++ src/test/java/de/workshops/bookshelf/BookshelfApplicationTests.java
@@ -0,0 +1,12 @@
+package de.workshops.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
